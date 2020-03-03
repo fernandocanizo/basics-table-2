068e9efab1a1e90fa59c97acbf37aba2a6fe5128
Fernando Lucio Canizo <fernandocanizo@gmail.com>
2020-03-03 16:56:06 -0300

    Add initial files from @pinguxx
    Downloads project from https://stackblitz.com/edit/basics-table-2 and
    adds them here.
    
    Doesn't work. Initial HTML is shown, but table is not built.

---
+++ index.js
@@ -0,0 +1,43 @@
+import './style.css';
+import { bind, wire } from 'hyperhtml/esm';
+
+const Table = {
+  html: bind(document.getElementById('app')),
+  data: [{ label: 'one', value: 1 }, { label: 'two', value: 2 }, { label: 'three', value: 3 }],
+  state: {
+    sorted: ''
+  },
+  handleEvent(e) {
+    //stop the regular link behaviour
+    e.preventDefault();
+    //get the current link
+    const link = e.target;
+    //read the attribute data-target, this will tell use how to sort the ary
+    const attr = link.dataset.target;
+    //check if the user clicked on the same attr
+    let asc = this.state.sorted === attr;
+    //simple sort, reverse the sort if asc is true
+    this.data.sort((a, b) => (''+a[attr]).localeCompare(''+b[attr]) * (asc ? 1 : -1));
+    //update the sorted attr
+    this.state.sorted = asc ? '' : attr;
+    //re-render
+    this.render();
+  },
+  render() {
+    return this.html`
+    <table class="table table-striped table-bordered table-sm">
+      <thead>
+        <tr>
+          <th><a onclick="${this}" data-target="label" href="#">Label</a></th>
+          <th><a onclick="${this}" data-target="value" href="#">Value</a></th>
+        </tr>
+      </thead>
+      <tbody>
+        ${this.data.map(obj => wire(obj) `<tr><td>${obj.label}</td><td>${obj.value}</td></tr>`)}
+      </tbody>
+    </table>
+  `;
+  }
+}
+
+Table.render();
